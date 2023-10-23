96e4519a905a48a73c6789345f0cb42adda02d6f
dostonradjabov <radjabovdoston0809@gmail.com>
2023-10-23 17:27:12 +0500

    code

--- Index07.py
+++ Index07.py
@@ -6,4 +6,9 @@ def main(s,n):
     Returns:
         str: answer
     """
-    return
+    if len(s)>=n:
+        return s[n]
+    else:
+        return False
+print(main("uz",4))
+print(main("good",3))
